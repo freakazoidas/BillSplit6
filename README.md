a7f94b379c4b66024bdb9c44c8350b9f94dd62d8
freakazoidas <freakazoidas@gmail.com>
2023-01-25 18:55:56 +0200

    going back to windows

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
-# BillSplit6  
-  
+# BillSplit6
+
 How to run:  
-$ . auth/Scripts/activate  
+$ . auth/Scripts/activate (for windows)
+$ . auth/bin/activate (for ubuntu)
 $ pip install -r requirements.txt  
 $ export FLASK_APP=project  
-$ flask run  
-  
+$ flask run
+
 p.s. I see 2 branches in Sourcetree, 1 branch in Git. No idea why. Will try to update and fix it.  
 ![image](https://user-images.githubusercontent.com/26854208/214225750-8464f686-23da-49c9-8c99-bcf8d784fc8c.png)
+
+Groups db:
+
+![image](https://user-images.githubusercontent.com/26854208/214595857-e6de2c11-5436-47a6-88a2-bd5c1a0af89b.png)
+
+CSS: Bulma
--- project/main.py
+++ project/main.py
@@ -1,7 +1,18 @@
 from flask import Blueprint, render_template
 from flask_login import current_user, login_required
+# 
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+# 
+engine = create_engine('sqlite:///db.sqlite')
 
+Session = sessionmaker(bind=engine)
+session = Session()
+# 
 from . import db
+
+
 
 main = Blueprint('main', __name__)
 
@@ -13,3 +24,8 @@ def index():
 @login_required
 def profile():
     return render_template('profile.html', name=current_user.name)
+
+@main.route('/groups')
+@login_required
+def groups():
+    return render_template('groups.html', name=current_user.name)
