2011a09d21f59cfcda17c50ee9c2118618ae3794
freakazoidas <freakazoidas@gmail.com>
2023-01-25 17:35:56 +0200

    groups route added in main

--- project/models.py
+++ project/models.py
@@ -9,10 +9,10 @@ class User(UserMixin, db.Model):
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
     
-class Group_names(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
-    group_name = db.Column(db.String(100), unique=True)
+# class Group_names(UserMixin, db.Model):
+#     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+#     group_name = db.Column(db.String(100), unique=True)
 
-class Group_intermediary(UserMixin, db.Model):
-    user_id = db.Column(db.Integer, models.ForeignKey("models.User", verbose_name=_(""), on_delete=models.CASCADE)=True) # primary keys are required by SQLAlchemy
-    group_id = db.Column(db.String(100), unique=True)
+# class Group_intermediary(UserMixin, db.Model):
+#     user_id = db.Column(db.Integer, models.ForeignKey("models.User", verbose_name=_(""), on_delete=models.CASCADE)=True) # primary keys are required by SQLAlchemy
+#     group_id = db.Column(db.String(100), unique=True)
