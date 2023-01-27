3287641b6f008fe667001ab33c6779d2f2859e62
freakazoidas <freakazoidas@gmail.com>
2023-01-27 11:56:35 +0200

    created classes for all tables

--- project/models.py
+++ project/models.py
@@ -2,6 +2,15 @@ from flask_login import UserMixin
 
 from . import db
 
+from sqlalchemy import ForeignKey
+from sqlalchemy import Integer
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import relationship
+
+
+
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
@@ -9,6 +18,22 @@ class User(UserMixin, db.Model):
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
     
+class BillGroups (db.Model):
+    __tablename__='bill_groups'
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    group_name = db.Column(db.String(255), unique=True)
+
+class BillGroupIntermediary (db.Model):
+    __tablename__ = 'group_user_intermediary'
+    user_id = db.Column (db.Integer), db.ForeignKey('user.id')
+    group_id = db.Column (db.Integer), db.ForeignKey('bill_groups.id')
+
+class IndividualBill (db.Model):
+    __tablename__ = 'individual_bill'
+    bill_id = db.Column (db.Integer), db.ForeignKey('bill_groups.id')
+    bill_item = db.Column(db.String(255))
+
+
 # class Group_names(UserMixin, db.Model):
 #     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
 #     group_name = db.Column(db.String(100), unique=True)
