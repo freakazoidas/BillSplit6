5ec723a9a3d4dbdbbdab07ee39287df3e5afa888
freakazoidas <freakazoidas@gmail.com>
2023-01-27 18:42:17 +0200

    working LOGIN and REGISTRATION

--- project/models.py
+++ project/models.py
@@ -5,8 +5,8 @@ from . import db
 from sqlalchemy import ForeignKey
 from sqlalchemy import Integer
 from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
-from sqlalchemy.orm import DeclarativeBase
+# from sqlalchemy.orm import mapped_column
+# from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import relationship
 
 
@@ -18,26 +18,17 @@ class User(UserMixin, db.Model):
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
     
-class BillGroups (db.Model):
-    __tablename__='bill_groups'
-    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
-    group_name = db.Column(db.String(255), unique=True)
-
-class BillGroupIntermediary (db.Model):
-    __tablename__ = 'group_user_intermediary'
-    user_id = db.Column (db.Integer), db.ForeignKey('user.id')
-    group_id = db.Column (db.Integer), db.ForeignKey('bill_groups.id')
-
-class IndividualBill (db.Model):
-    __tablename__ = 'individual_bill'
-    bill_id = db.Column (db.Integer), db.ForeignKey('bill_groups.id')
-    bill_item = db.Column(db.String(255))
-
-
-# class Group_names(UserMixin, db.Model):
+# class BillGroups (db.Model):
+#     __tablename__='bill_groups'
 #     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
-#     group_name = db.Column(db.String(100), unique=True)
+#     group_name = db.Column(db.String(255), unique=True)
 
-# class Group_intermediary(UserMixin, db.Model):
-#     user_id = db.Column(db.Integer, models.ForeignKey("models.User", verbose_name=_(""), on_delete=models.CASCADE)=True) # primary keys are required by SQLAlchemy
-#     group_id = db.Column(db.String(100), unique=True)
+# class BillGroupIntermediary (db.Model):
+#     __tablename__ = 'group_user_intermediary'
+#     user_id = db.Column (db.Integer), db.ForeignKey('user.id')
+#     group_id = db.Column (db.Integer), db.ForeignKey('bill_groups.id')
+
+# class IndividualBill (db.Model):
+#     __tablename__ = 'individual_bill'
+#     bill_id = db.Column (db.Integer), db.ForeignKey('bill_groups.id')
+#     bill_item = db.Column(db.String(255))
