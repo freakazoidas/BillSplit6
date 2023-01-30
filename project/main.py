79ea9e43836f49e534c35d82563c5b06e22b29e2
freakazoidas <freakazoidas@gmail.com>
2023-01-30 15:50:37 +0200

    group/group_IDshowing individual billcontents

--- project/main.py
+++ project/main.py
@@ -1,12 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import current_user, login_required
 # 
-from sqlalchemy import (Column, Float, ForeignKey, Integer, String,
-                        create_engine)
+from sqlalchemy import (Column, Float, ForeignKey, Integer, String, create_engine)
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 
-from project.models import BillGroupIntermediary, BillGroups
+from project.models import BillGroupIntermediary, BillGroups, IndividualBill
 
 # 
 engine = create_engine('sqlite:///db.sqlite')
@@ -27,11 +26,30 @@ def index():
 def profile():
     return render_template('profile.html', name=current_user.name)
 
-@main.route('/groups')
+@main.route('/groups', methods=['GET', 'POST'])
 @login_required
 def groups():
     user_id = current_user.id
     user_groups = BillGroupIntermediary.query.filter_by(user_id=user_id).all()
     user_group_ids = [group.group_id for group in user_groups]
     groups = BillGroups.query.all()
+    
+    if request.method == 'POST':
+        group_name = request.form['group_name']
+        new_group = BillGroups(group_name=group_name)
+        db.session.add(new_group)
+        db.session.commit()
+        # Create a new entry in the group_user_intermediary table to link the new group to the current user
+        group_intermediary = BillGroupIntermediary(user_id=current_user.id, group_id=new_group.id)
+        db.session.add(group_intermediary)
+        db.session.commit()
+
     return render_template('groups.html', groups=groups, user_group_ids=user_group_ids)
+
+
+@main.route('/groups/<int:bill_id>', methods=['GET', 'POST'])
+@login_required
+def bill(bill_id):
+    bills = IndividualBill.query.filter_by(bill_id=bill_id).all()
+    return render_template('bill.html', bills=bills)
+
